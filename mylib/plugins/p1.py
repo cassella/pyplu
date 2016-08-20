a8412fe95aa1fa3da4fb6a07293017f67e95f87a
Paul Cassella <gitgitgit@manetheren.bigw.org>
2016-08-20 12:57:26 -0700

    make a general add_reportobj_args

--- mylib/plugins/p1.py
+++ mylib/plugins/p1.py
@@ -7,10 +7,6 @@ default_p1_fielddescs = ["a_str", "an_int"]
 
 p1 = reportobj("p1", default_p1_fielddescs)
 
-def add_p1_fields(arg):
-    return p1.add_fields(arg)
-
-
 field(p1, "a_str", "a_str", "STR", 8, get_a_str)
 field(p1, "a_strtwo", "a_strtwo", "STR2", 8, get_a_str)
 field(p1, "an_int", "an_int", "I", 2, get_an_int)
@@ -19,20 +15,26 @@ field(p1, "an_inttwo-hex", "an_inttwo", "INT2", 4, get_an_int_hex)
 
 default_p1_fields = p1.descs_to_fields(default_p1_fielddescs)
 
-def replace_p1_fields(arg):
-    return (p1, p1.descs_to_fields(arg.split(",")))
+def add_reportobj_args(parser, ro, default_fields):
+    def add_ro_fields(arg):
+        return ro.add_fields(arg)
+
+    def replace_ro_fields(arg):
+        return (ro, ro.descs_to_fields(arg.split(",")))
+
+    parser.add_argument("--" + ro.name, action="append_const", dest="report",
+                        help="show {ro} default fields".format(ro=ro.name),
+                        const=(ro, default_p1_fields))
+    parser.add_argument("--" + ro.name + "+", action="append", dest="report",
+                        help="show {ro} with additional fields".format(ro=ro.name),
+                        metavar="EXTRA",
+                        type=add_ro_fields)
+    parser.add_argument("--" + ro.name + "-", action="append", dest="report",
+                        help="show {ro} with only these fields".format(ro=ro.name),
+                        metavar="FIELDS",
+                        type=replace_ro_fields)
 
 def add_p1_args(parser):
-    parser.add_argument("--p1", action="append_const", dest="report",
-                        help="show p1 default fields",
-                        const=(p1, default_p1_fields))
-    parser.add_argument("--p1+", action="append", dest="report",
-                        help="show p1 with additional fields",
-                        metavar="EXTRA",
-                        type=add_p1_fields)
-    parser.add_argument("--p1-", action="append", dest="report",
-                        help="show p1 with only these fields",
-                        metavar="FIELDS",
-                        type=replace_p1_fields)
+    return add_reportobj_args(parser, p1, default_p1_fields)
 
 Plugin("p1", add_p1_args)
