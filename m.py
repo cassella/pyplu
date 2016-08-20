ddf297ead4229345bf7f034aca82d88231f7e4c8
Paul Cassella <gitgitgit@manetheren.bigw.org>
2016-08-20 12:07:08 -0700

    Make p1 more general

--- m.py
+++ m.py
@@ -4,6 +4,17 @@ import argparse
 
 import mylib
 
+class testobj:
+    def __init__(self, a_str, a_strtwo, an_int, an_inttwo):
+        self.a_str = a_str
+        self.a_strtwo = a_strtwo
+        self.an_int = an_int
+        self.an_inttwo = an_inttwo
+
+testobjs = [
+    testobj("flumpf", "whimper", 42, 4242),
+    testobj("MIMSY", "GIMBLE", 77, 7777),
+]
 
 parser = argparse.ArgumentParser(description="oh hi")
 
@@ -11,5 +22,6 @@ mylib.add_args(parser)
 
 args = parser.parse_args()
 
-for (r,f) in args.report:
-    r("singleton", f)
+for o in testobjs:
+    for (r,f) in args.report:
+        r(o, f)
--- mylib/plugins/p1.py
+++ mylib/plugins/p1.py
@@ -2,16 +2,31 @@
 
 from mylib import Plugin
 
+class field:
+    def __init__(self, fields, name, hdrname, width, getstr):
+        self.name = name
+        self.hdrname = hdrname
+        self.width = width
+        self.getstr = getstr
+        fields[name] = self
 
-def default_p1_formatter(p1):
-    pass
+def get_a_str(p1, name):
+    return p1.__dict__[name]
 
-def show_p1(addr, formatter):
-    print "{addr} as a p1".format(addr=addr)
+p1_fields = {}
+
+field(p1_fields, "a_str", "STR", 8, get_a_str)
+
+
+default_p1_fields=[p1_fields["a_str"]]
+
+def show_p1(p1, fields):
+    for f in fields:
+        print f.getstr(p1, f.name)
 
 
 def add_p1_args(parser):
     parser.add_argument("--p1", action="append_const", dest="report",
-                        const=(show_p1,default_p1_formatter))
+                        const=(show_p1,default_p1_fields))
 
 Plugin("p1", add_p1_args)
