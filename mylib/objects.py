2b3b84ac76280d45192b3b1470bc8f921b75cc58
Paul Cassella <gitgitgit@manetheren.bigw.org>
2016-08-20 12:49:12 -0700

    move reportobj to objects module

--- mylib/objects.py
+++ mylib/objects.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 class field:
-    def __init__(self, fields, desc, name, hdrname, width, getstr):
+    def __init__(self, reportobj, desc, name, hdrname, width, getstr):
         self.name = name
         self.desc = desc
         self.hdrname = hdrname
         self.width = width
         self.getstr = getstr
-        fields[desc] = self
+        reportobj.fields[desc] = self
 
 def get_a_str(p1, name):
     return p1.__dict__[name]
@@ -17,3 +17,25 @@ def get_an_int(p1, name):
 
 def get_an_int_hex(p1, name):
     return "{:x}".format(p1.__dict__[name])
+
+
+class reportobj:
+    def __init__(self, name, default_fielddescs):
+        self.fields = {}
+        self.name = name
+        self.default_fielddescs = default_fielddescs
+
+    def desc_to_field(self, desc):
+        return self.fields[desc]
+
+    def descs_to_fields(self, descs):
+        return map(self.desc_to_field, descs)
+
+    def show(self, obj, fields):
+        for f in fields:
+            print f.getstr(obj, f.name),
+        print
+
+    def add_fields(self, arg):
+        fieldnames = self.default_fielddescs + arg.split(",")
+        return (self, self.descs_to_fields(fieldnames))
--- mylib/plugins/p1.py
+++ mylib/plugins/p1.py
@@ -3,40 +3,19 @@
 from mylib import Plugin
 from mylib.objects import *
 
-
 default_p1_fielddescs = ["a_str", "an_int"]
 
-class reportobj:
-    def __init__(self, name):
-        self.fields = {}
-        self.name = name
-
-    def desc_to_field(self, desc):
-        return self.fields[desc]
-
-    def descs_to_fields(self, descs):
-        return map(self.desc_to_field, descs)
-
-    def show(self, obj, fields):
-        for f in fields:
-            print f.getstr(obj, f.name),
-        print
-
-    def add_fields(self, arg):
-        fieldnames = default_p1_fielddescs + arg.split(",")
-        return (self, self.descs_to_fields(fieldnames))
-
-p1 = reportobj("p1")
+p1 = reportobj("p1", default_p1_fielddescs)
 
 def add_p1_fields(arg):
     return p1.add_fields(arg)
 
 
-field(p1.fields, "a_str", "a_str", "STR", 8, get_a_str)
-field(p1.fields, "a_strtwo", "a_strtwo", "STR2", 8, get_a_str)
-field(p1.fields, "an_int", "an_int", "I", 2, get_an_int)
-field(p1.fields, "an_inttwo", "an_inttwo", "INT2", 4, get_an_int)
-field(p1.fields, "an_inttwo-hex", "an_inttwo", "INT2", 4, get_an_int_hex)
+field(p1, "a_str", "a_str", "STR", 8, get_a_str)
+field(p1, "a_strtwo", "a_strtwo", "STR2", 8, get_a_str)
+field(p1, "an_int", "an_int", "I", 2, get_an_int)
+field(p1, "an_inttwo", "an_inttwo", "INT2", 4, get_an_int)
+field(p1, "an_inttwo-hex", "an_inttwo", "INT2", 4, get_an_int_hex)
 
 default_p1_fields = p1.descs_to_fields(default_p1_fielddescs)
 
